02f44121c027ce744dfc28d782ca409fe825dd4b
Oleg Rashchinksy <oleg.rashchinksy@sharpgaming.com>
2025-01-15 20:20:27 +0100

    feat: update helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,31 +1,18 @@
 import { access } from 'node:fs/promises';
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { join } from 'node:path';
 
-export const getPath = (currentPath, folderName, fileName = '') => {
-  try{
-    const currentFile = fileURLToPath(currentPath);
-    const currentDirName = dirname(currentFile);
-
-    return join(currentDirName, folderName, fileName);
-  } catch (err) {
-    throw err;
-  }
-}
+export const getPath = (currentPath, folderName, fileName = '') =>
+  join(currentPath, folderName, fileName);
 
 export const doesPathExist = async (path) => {
   try {
     await access(path);
 
     return true;
-  } catch(err) {
-    if(err.code === 'ENOENT') {
+  } catch (err) {
+    if (err.code === 'ENOENT') {
       return false;
-    } 
+    }
     throw err;
   }
-}
-
-export const operationFail = () => {
-  throw new Error('FS operation failed');
-}
+};
