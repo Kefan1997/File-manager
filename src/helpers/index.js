81efca71480112379c67a79eb82e0193e722f210
Oleg Rashchinksy <oleg.rashchinksy@sharpgaming.com>
2025-01-16 20:52:42 +0100

    feat: update helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,18 +1,4 @@
-import { access } from 'node:fs/promises';
 import { join } from 'node:path';
 
 export const getPath = (currentPath, folderName, fileName = '') =>
   join(currentPath, folderName, fileName);
-
-export const doesPathExist = async (path) => {
-  try {
-    await access(path);
-
-    return true;
-  } catch (err) {
-    if (err.code === 'ENOENT') {
-      return false;
-    }
-    throw err;
-  }
-};
