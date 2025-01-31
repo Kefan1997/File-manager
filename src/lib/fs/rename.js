c49e13de8356bf0a34e3bc724b763bc7e69ab1b7
Oleg Rashchinksy <oleg.rashchinksy@sharpgaming.com>
2025-01-31 15:42:02 +0100

    feat: remove try catch

--- src/lib/fs/rename.js
+++ src/lib/fs/rename.js
@@ -2,11 +2,7 @@ import { rename } from 'node:fs/promises';
 import path from 'node:path';
 
 export default async function renameFn(pathToFile, newFileName) {
-  try {
     const resolvedPath = path.resolve(pathToFile);
 
     await rename(resolvedPath, newFileName);
-  } catch (err) {
-    throw err;
-  }
 }
